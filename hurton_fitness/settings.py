70667fadaec97277e35085056ff50e911332b9ce
alandoherty95 <alandoherty95@gmail.com>
2021-09-30 16:36:47 +0000

    Created about app, added urls.py and updated settings.py

--- hurton_fitness/settings.py
+++ hurton_fitness/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'contact',
     'gallery',
+    'about',   
     'crispy_forms',
 ]
 
--- hurton_fitness/urls.py
+++ hurton_fitness/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('contact/', include('contact.urls')),
     path('gallery/', include('gallery.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
