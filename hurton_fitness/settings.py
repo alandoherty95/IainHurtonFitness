7041601d056ee0415dcdc5286d9b84204bd83188
alandoherty95 <alandoherty95@gmail.com>
2022-01-05 13:54:36 +0000

    Settings.py updated

--- hurton_fitness/settings.py
+++ hurton_fitness/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@j8+l!f*i+gs548%5yye@hwruuaatqp_!2tw%gy$-($v4fa28'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
